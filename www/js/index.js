59a323d7a981857e7aa25e2151f7b125aa74b77f
vito <vito@vito.com>
2015-04-04 17:48:14 -0400

    added mbtile file

--- www/js/index.js
+++ www/js/index.js
@@ -128,33 +128,13 @@ console.log(mapHeight);
 function testDb() {
 
 console.log("test db");
-	var db = window.sqlitePlugin.openDatabase({name: "my.db", createFromLocation: 1});
+	var db = window.sqlitePlugin.openDatabase({name: "test.mbtiles"});
 
 	  db.transaction(function(tx) {
-	    tx.executeSql('DROP TABLE IF EXISTS test_table');
-	    tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
-
-	    // demonstrate PRAGMA:
-	    db.executeSql("pragma table_info (test_table);", [], function(res) {
-	      console.log("PRAGMA res: " + JSON.stringify(res));
-	    });
-
-	    tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-alert("inserted into test_table");
-	      console.log("insertId: " + res.insertId + " -- probably 1");
-	      console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
-
-	      db.transaction(function(tx) {
 		tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
 		  console.log("res.rows.length: " + res.rows.length + " -- should be 1");
 		  console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
 		});
-	      });
-
-	    }, function(e) {
-	alert("there is an error trying to insert db");
-	      console.log("ERROR: " + e.message);
-	    });
 	});
 }
 
