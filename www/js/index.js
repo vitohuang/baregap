b38d5a9203b620a4f5bcfa9db880f8ef9f91db18
vito <vito@vito.com>
2015-04-04 18:55:47 -0400

    jfdgkls

--- www/js/index.js
+++ www/js/index.js
@@ -39,18 +39,15 @@ var app = {
         app.receivedEvent('deviceready');
 
 	// Init map
-	initMap();
+	//initMap();
 
+go();
 	// Trying to get the db
 	testDb();
 
 	// Test the file system path
 	testFs();
 
-	console.log("cordova file");
-	alert("cordova file doc:"+cordova.file.applicationStorageDirectory);
-	alert("cordova file doc:"+cordova.file.applicationDirectory);
-	alert("cordova file data directory:"+cordova.file.dataDirectory);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -162,4 +159,67 @@ function gotFs(fileSystem) {
 		}
 	});
 	*/
+}
+
+
+var localFileName;	// the filename of the local mbtiles file
+var remoteFile;		// the url of the remote mbtiles file to be downloaded
+var msg;			// the span to show messages
+
+localFileName = 'test.mbtiles';
+remoteFile = 'http://178.62.13.207/test.mbtiles';
+
+function go() {
+	var fs;				// file system object
+	var ft;				// TileTransfer object
+
+	msg = document.getElementById('message');
+	
+	console.log('requesting file system...');
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+		console.log('file system retrieved.');
+		fs = fileSystem;
+
+		// check to see if files already exists
+		var file = fs.root.getFile(localFileName, {create: false}, function () {
+			// file exists
+			console.log('exists');
+
+			msg.innerHTML = 'File already exists on device. Building map...';
+
+			buildMap();
+		}, function () {
+			// file does not exist
+			console.log('does not exist');
+
+			msg.innerHTML = 'Downloading file...';
+
+			console.log('downloading sqlite file...');
+			ft = new FileTransfer();
+			ft.download(remoteFile, fs.root.fullPath + '/' + localFileName, function (entry) {
+				console.log('download complete: ' + entry.fullPath);
+
+				buildMap();
+
+			}, function (error) {
+				console.log('error with download', error);
+			});
+		});
+	});
+}
+
+function buildMap() {
+resizeMap();
+	var db = new SQLitePlugin(localFileName);
+
+	document.body.removeChild(msg);
+
+	var map = new L.Map('map', {
+		center: new L.LatLng(40.6681, -111.9364),
+		zoom: 11
+	});
+
+	var lyr = new L.TileLayer.MBTiles('', {maxZoom: 2, scheme: 'tms'}, db);
+
+	map.addLayer(lyr);
 }
