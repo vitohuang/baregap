50e2ab95d265c0206d8ff5468cb2ff3d3d82c7f5
vito <vito@vito.com>
2015-04-06 09:10:01 -0400

    jdfkljdf

--- www/js/index.js
+++ www/js/index.js
@@ -247,7 +247,39 @@ var i = 1;
 alert("build map:"+dbFileName);
 resizeMap();
 	var db = window.sqlitePlugin.openDatabase({name: dbFileName, androidLockWorkaround: 1});
+db.transaction(function(tx) {
+alert("going to do the transaction");
+    tx.executeSql('DROP TABLE IF EXISTS test_table');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
 
+    // demonstrate PRAGMA:
+    db.executeSql("pragma table_info (test_table);", [], function(res) {
+alert("executed sql: pragma");
+      console.log("PRAGMA res: " + JSON.stringify(res));
+    });
+
+    tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+alert("stuff inserted");
+      console.log("insertId: " + res.insertId + " -- probably 1");
+      console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+
+      db.transaction(function(tx) {
+        tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+alert("result from select");
+alert(JSON.stringify(res));
+
+          console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+          console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+        });
+      });
+
+
+    }, function(e) {
+      console.log("ERROR: " + e.message);
+    });
+  });
+
+/*
 db.transaction(function(tx) {
 alert("going to do the transaction");
     // demonstrate PRAGMA:
@@ -291,6 +323,7 @@ alert(abc);
 	});
 
   });
+*/
 
 
 
